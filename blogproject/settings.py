ad5ed426194c6320c388e9fe094b0e5fe61e58d7
ltiisidii <jonatan.wb@gmail.com>
2022-06-06 15:21:39 -0300

    [Fix] base dir settings.py change

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -14,6 +14,9 @@ DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
